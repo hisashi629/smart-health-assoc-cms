aec472bc461f0224233a46e0fa3f9bc68c0b7f16
hisash <hisashi629@gmail.com>
2019-01-21 01:07:44 +0900

    fix preview

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -5,6 +5,8 @@ export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
+    let dl_files = entry.getIn(["data", "dl_files"]);
+    let images = entry.getIn(["data", "images"]);
 
     return <div className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
@@ -17,6 +19,22 @@ export default class PostPreview extends React.Component {
         { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
         { widgetFor("body") }
       </div>
+      <ul class="dl_list">
+        { dl_files && dl_files.map((value, index) => (
+          <li key={index.toString()}>{value.get('name')}<button><a href="{value.get('url')}" target="_blank">ダウンロード</a></button></li>
+        ))}
+      </ul>
+      <ul class="showcase">
+        {images && images.map((value, index) => (
+          <li key={index.toString()}>
+            <figure>
+              <img src={value.get('url')}></img>
+              <figcaption>{value.get('caption')}</figcaption>
+            </figure>
+          </li>
+        ))}
+      </ul>
+
     </div>;
   }
 }
